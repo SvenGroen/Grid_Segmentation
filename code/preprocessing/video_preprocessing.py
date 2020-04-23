236112e1d560f8b810c937ccf0efcb1820dcbf6d
SvenGroen <sgroen@uos.de>
2020-04-23 11:05:11 +0200

    created train.py

--- code/preprocessing/video_preprocessing.py
+++ code/preprocessing/video_preprocessing.py
@@ -7,11 +7,15 @@ import numpy as np
 import os
 from collections import defaultdict
 from pathlib import Path
+# This file extracts all frames from all videos in vid_path and saves them accordingly
+# label frames are created based on the "blackness" of each pixel
+
 
 vid_path = Path(Path.cwd()) / "data/Videos/Examples"
-backgroundname = Path("no_bg")
+backgroundname = Path("raw")
 for vid in vid_path.glob("*.mp4"):
     cap = cv2.VideoCapture(str(vid))
+    
     name = Path(vid.stem)
     frame_count = 0
     label_path = (Path.cwd() / Path("data/Images/Examples")/ backgroundname / name / Path("labels/"))
@@ -21,7 +25,8 @@ for vid in vid_path.glob("*.mp4"):
     print("Video: ", name)
     while cap.isOpened():
         ret, frame = cap.read()
-        fname = str(name) +"_"+ str(frame_count) + ".png"
+        num_filler = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
+        fname = str(name) +"_"+ str(frame_count).zfill(num_filler) + ".jpg"
         if ret:
             label = (np.any(frame, axis=2, keepdims=False) * 255.)
             cv2.imwrite(str(frame_path) + "\\"+ fname, frame)
@@ -30,4 +35,5 @@ for vid in vid_path.glob("*.mp4"):
         else:
             break
     cap.release()
+
 print("All Videos processed")
