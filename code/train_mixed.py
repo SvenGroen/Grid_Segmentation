2e4a6454f6bcd2347391ec6d84e84252ea88066b
SvenGroen <sgroen@uos.de>
2020-05-05 12:54:40 +0200

    trained ICNet

--- code/train_mixed.py
+++ code/train_mixed.py
@@ -18,7 +18,7 @@ from collections import defaultdict
 
 print("Python Script Start")
 
-model = "ICNet"  # Options available: "UNet", "Deep_Res101", "ConvSame_3", "Deep_Res50", "Deep+_mobile", "ICNet"
+model = "Deep+_mobile"  # Options available: "UNet", "Deep_Res101", "ConvSame_3", "Deep_Res50", "Deep+_mobile", "ICNet"
 criterion = F.cross_entropy
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 norm_ImageNet = False
@@ -57,13 +57,13 @@ transform = [T.RandomPerspective(distortion_scale=0.1), T.ColorJitter(0.5, 0.5, 
              T.RandomAffine(degrees=10, scale=(1, 1.5)), T.RandomGrayscale(p=0.1), T.RandomGrayscale(p=0.1),
              T.RandomHorizontalFlip(p=0.7)]
 dataset = NY_mixed(transforms=transform)  # <--- SET DATASET
-batch_size = 6  # <--- SET BATCHSIZE
+batch_size = 10  # <--- SET BATCHSIZE
 if model == "Deep_Res101" or model == "Deep_Res50":
     assert batch_size > 1, "Batch size must be larger 1 for Deeplab to work"
-lr = 1e-03  # <--- SET LEARNINGRATE
+lr = 1e-04  # <--- SET LEARNINGRATE
 num_epochs = 100  # <--- SET NUMBER OF EPOCHS
 start_epoch = 0
-save_freq = 10
+save_freq = 2
 
 train_loader = DataLoader(dataset=dataset, batch_size=batch_size)
 train_name = model + "_bs" + str(batch_size) + "_lr" + format(lr, ".0e") + "_ep" + str(
